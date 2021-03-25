2ba698b6668000e94cfe265897b715041887a4cb
michals3r3k <serek.michal0827@gmail.com>
2021-03-25 17:21:08 +0100

    HotFix

--- src/main/java/pl/odum/workflowodum/controller/DocController.java
+++ src/main/java/pl/odum/workflowodum/controller/DocController.java
@@ -1,14 +1,12 @@
 package pl.odum.workflowodum.controller;
 
 import lombok.AllArgsConstructor;
-import org.apache.commons.io.FileUtils;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import pl.odum.workflowodum.model.Doc;
-import pl.odum.workflowodum.service.DocStorageService;
+import pl.odum.workflowodum.service.DocService;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
@@ -16,24 +14,24 @@ import java.util.List;
 @Controller
 @AllArgsConstructor
 public class DocController {
-    private final DocStorageService docStorageService;
+    private final DocService docService;
 
     @GetMapping("/home")
     public String test(Model model) {
-        List<Doc> docs = docStorageService.getFiles();
+        List<Doc> docs = docService.getFiles();
         model.addAttribute("docs", docs);
         return "doc";
     }
 
     @PostMapping("/upload")
     public String uploadFilePost(@RequestParam("files") List<MultipartFile> files) {
-        docStorageService.saveFilesFromMultiPart(files);
-        return "redirect:/";
+        docService.saveFilesFromMultiPart(files);
+        return "redirect:/home";
     }
 
     @GetMapping("/download/{id}")
     public void downloadFileGet(@PathVariable Long id, HttpServletResponse response) {
-        Doc doc = docStorageService.findById(id);
-        docStorageService.download(doc, response);
+        Doc doc = docService.findById(id);
+        docService.download(doc, response);
     }
 }
