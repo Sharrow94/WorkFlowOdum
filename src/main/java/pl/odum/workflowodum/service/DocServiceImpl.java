7fb21ab395fc4fe0bffb8c7ad43b1998e240e923
Maciej Roszyk <maciej.roszyk@gmail.com>
2021-04-07 16:04:56 +0200

    MR/SmallHotFix

--- src/main/java/pl/odum/workflowodum/service/DocServiceImpl.java
+++ src/main/java/pl/odum/workflowodum/service/DocServiceImpl.java
@@ -19,6 +19,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.stream.Collectors;
