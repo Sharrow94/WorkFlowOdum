abe094ef3c797b24ed146a8505c2bef0255151dc
michals3r3k <serek.michal0827@gmail.com>
2021-04-13 10:10:19 +0200

    Sending form

--- src/main/java/pl/odum/workflowodum/controller/MeetingController.java
+++ src/main/java/pl/odum/workflowodum/controller/MeetingController.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.odum.workflowodum.model.ClientEmployee;
 import pl.odum.workflowodum.model.Meeting;
 import pl.odum.workflowodum.model.User;
 import pl.odum.workflowodum.service.ClientService;
+import pl.odum.workflowodum.service.DocService;
 import pl.odum.workflowodum.service.MeetingService;
 import pl.odum.workflowodum.service.UserService;
 
@@ -21,6 +24,7 @@ public class MeetingController {
     private final MeetingService meetingService;
     private final UserService userService;
     private final ClientService  clientService;
+    private final DocService docService;
 
     @RequestMapping("/all/{userId}")
     public String showMeetingsForUser(Model model,@PathVariable("userId")Long id){
@@ -40,7 +44,7 @@ public class MeetingController {
     public String createMeeting(Meeting meeting){
         meeting.setUser(userService.findByUserName(SecurityContextHolder.getContext().getAuthentication().getName()));
         meetingService.save(meeting);
-        return "redirect:/meeting/all";
+        return "redirect:/app/meeting/all/"+meeting.getClient().getId();
     }
 
     @RequestMapping("/edit/{id}")
@@ -54,6 +58,20 @@ public class MeetingController {
     public String editMeeting(Meeting meeting){
         meetingService.save(meeting);
         return "redirect:/meeting/all";
+    }
+
+    @GetMapping("/{meetingId}/send-attachment/{docUUID}")
+    public String showClientsGet(Model model, @PathVariable String docUUID, @PathVariable Long meetingId){
+        model.addAttribute("meeting", meetingService.findById(meetingId));
+        model.addAttribute("doc", docService.findByUuid(docUUID));
+        model.addAttribute("clientEmployee", new ClientEmployee());
+        return "meeting/sendToEmployee";
+    }
+
+    @PostMapping("/{meetingId}/send-attachment/{docUUID}")
+    public String showClientsPost(@PathVariable String docUUID, @PathVariable Long meetingId){
+
+        return "meeting/sendToEmployee";
     }
 
 }
